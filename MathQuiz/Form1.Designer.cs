e40289c2400ae7ef2b3f63433d61723bfd96b63c
Blaine Curtis <bluebomber621@gmail.com>
2025-06-02 12:21:30 -0600

    Add project files.

---
+++ MathQuiz/Form1.Designer.cs
@@ -0,0 +1,181 @@
+﻿namespace MathQuiz
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            components = new System.ComponentModel.Container();
+            timeLabel = new Label();
+            label1 = new Label();
+            equationLabel = new Label();
+            label2 = new Label();
+            label3 = new Label();
+            numericAnswer = new NumericUpDown();
+            continueButton = new Button();
+            buttonCont = new Button();
+            questionLabel = new Label();
+            timer = new System.Windows.Forms.Timer(components);
+            ((System.ComponentModel.ISupportInitialize)numericAnswer).BeginInit();
+            SuspendLayout();
+            // 
+            // timeLabel
+            // 
+            timeLabel.BorderStyle = BorderStyle.FixedSingle;
+            timeLabel.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            timeLabel.Location = new Point(266, 9);
+            timeLabel.Margin = new Padding(0, 0, 3, 0);
+            timeLabel.Name = "timeLabel";
+            timeLabel.Size = new Size(200, 30);
+            timeLabel.TabIndex = 0;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Segoe UI", 15F);
+            label1.Location = new Point(141, 5);
+            label1.Name = "label1";
+            label1.Size = new Size(122, 35);
+            label1.TabIndex = 1;
+            label1.Text = "Time Left:";
+            // 
+            // equationLabel
+            // 
+            equationLabel.Font = new Font("Segoe UI", 18F);
+            equationLabel.Location = new Point(38, 70);
+            equationLabel.Name = "equationLabel";
+            equationLabel.Size = new Size(400, 45);
+            equationLabel.TabIndex = 2;
+            equationLabel.Text = "?";
+            equationLabel.TextAlign = ContentAlignment.MiddleCenter;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(62, 165);
+            label2.Name = "label2";
+            label2.Size = new Size(0, 20);
+            label2.TabIndex = 3;
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Font = new Font("Segoe UI", 15F);
+            label3.Location = new Point(109, 150);
+            label3.Name = "label3";
+            label3.Size = new Size(101, 35);
+            label3.TabIndex = 4;
+            label3.Text = "Answer:";
+            label3.TextAlign = ContentAlignment.MiddleLeft;
+            // 
+            // numericAnswer
+            // 
+            numericAnswer.Font = new Font("Segoe UI", 14F);
+            numericAnswer.Location = new Point(216, 151);
+            numericAnswer.Maximum = new decimal(new int[] { 99999, 0, 0, 0 });
+            numericAnswer.Minimum = new decimal(new int[] { 99999, 0, 0, int.MinValue });
+            numericAnswer.Name = "numericAnswer";
+            numericAnswer.Size = new Size(150, 39);
+            numericAnswer.TabIndex = 5;
+            numericAnswer.ThousandsSeparator = true;
+            // 
+            // continueButton
+            // 
+            continueButton.Font = new Font("Segoe UI", 14F);
+            continueButton.Location = new Point(141, 228);
+            continueButton.Name = "continueButton";
+            continueButton.Size = new Size(205, 60);
+            continueButton.TabIndex = 6;
+            continueButton.Text = "Start the Quiz!";
+            continueButton.UseVisualStyleBackColor = true;
+            // 
+            // buttonCont
+            // 
+            buttonCont.Font = new Font("Segoe UI", 14F);
+            buttonCont.Location = new Point(125, 228);
+            buttonCont.Name = "buttonCont";
+            buttonCont.Size = new Size(224, 60);
+            buttonCont.TabIndex = 6;
+            buttonCont.Text = "Start the Quiz!";
+            buttonCont.UseVisualStyleBackColor = true;
+            buttonCont.Click += buttonCont_Click;
+            // 
+            // questionLabel
+            // 
+            questionLabel.AutoSize = true;
+            questionLabel.Font = new Font("Segoe UI", 15F);
+            questionLabel.ImageAlign = ContentAlignment.TopLeft;
+            questionLabel.Location = new Point(12, 5);
+            questionLabel.Name = "questionLabel";
+            questionLabel.Size = new Size(54, 35);
+            questionLabel.TabIndex = 7;
+            questionLabel.Text = "Q 0";
+            questionLabel.TextAlign = ContentAlignment.MiddleLeft;
+            // 
+            // timer
+            // 
+            timer.Enabled = true;
+            timer.Interval = 1000;
+            timer.Tick += timer_Tick;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(478, 349);
+            Controls.Add(questionLabel);
+            Controls.Add(buttonCont);
+            Controls.Add(continueButton);
+            Controls.Add(numericAnswer);
+            Controls.Add(label3);
+            Controls.Add(label2);
+            Controls.Add(equationLabel);
+            Controls.Add(label1);
+            Controls.Add(timeLabel);
+            FormBorderStyle = FormBorderStyle.Fixed3D;
+            MaximizeBox = false;
+            Name = "Form1";
+            Text = "Math Quiz";
+            Load += Form1_Load;
+            ((System.ComponentModel.ISupportInitialize)numericAnswer).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label timeLabel;
+        private Label label1;
+        private Label equationLabel;
+        private Label label2;
+        private Label label3;
+        private NumericUpDown numericAnswer;
+        private Button continueButton;
+        private Button buttonCont;
+        private Label questionLabel;
+        private System.Windows.Forms.Timer timer;
+    }
+}
---
+++ MathQuiz/Form1.cs
@@ -0,0 +1,107 @@
+namespace MathQuiz
+{
+    public partial class Form1 : Form
+    {
+        // Declare fields at the class level to ensure they are accessible throughout the class
+        private Random randomizer = new();
+        private int questionNum = 0;
+        private int timeLeft = 30;
+        private int questionAnswer = 0;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            timer.Enabled = false;
+        }
+
+        public void Quiz_Start()
+        {
+            // Start timer
+            timeLeft = 300;
+            timer.Enabled = true;
+            timer.Start();
+            timeLabel.Text = "5:00";
+
+            // Prep question number
+            questionNum = 49;
+        }
+
+        public int Generate_Question()
+        {
+            int eTypeSel1, ePart1, ePart2, eType1;
+                eTypeSel1 = 1 + ((int)Math.Floor((decimal)(questionNum / 5)));
+                eType1 = randomizer.Next(1, ((eTypeSel1 < 5) ? eTypeSel1 : 5 ));
+                if (eType1 == 4)
+                {
+                    ePart2 = randomizer.Next(2, 12);
+                    ePart1 = ePart2 * randomizer.Next(2, 2 + questionNum);
+                }
+                else
+                {
+                     ePart1 = (eType1 != 5) ? randomizer.Next(1, 5 + (int)(3 * Math.Round((double)questionNum / eType1))) : randomizer.Next(2, 6 + questionNum);
+                     ePart2 = (eType1 != 5) ? randomizer.Next(1, 5 + (int)(3 * Math.Round((double)questionNum / eType1))) : randomizer.Next(2, 3);
+                }
+                if (eType1 == 1) { equationLabel.Text = $"{ePart1} + {ePart2}"; return ePart1 + ePart2; }
+                else if (eType1 == 2) { equationLabel.Text = $"{ePart1} - {ePart2}"; return ePart1 - ePart2; }
+                else if (eType1 == 3) { equationLabel.Text = $"{ePart1} * {ePart2}"; return ePart1 * ePart2; }
+                else if (eType1 == 4) { equationLabel.Text = $"{ePart1} / {ePart2}"; return ePart1 / ePart2; }
+                else if (eType1 == 5) { equationLabel.Text = $"{ePart1} ^ {ePart2}"; return (int)Math.Pow(ePart1, ePart2); }
+                return 0;
+        }
+
+        private void buttonCont_Click(object sender, EventArgs e)
+        {
+            if (!timer.Enabled)
+            {
+                Quiz_Start();
+                buttonCont.Text = "Submit";
+                questionLabel.Text = "Q " + questionNum;
+                questionAnswer = Generate_Question();
+            }
+            else
+            {
+                if ((double)numericAnswer.Value == questionAnswer && timeLeft > 0)
+                {
+                    questionAnswer = 99999;
+                    numericAnswer.Value = 0;
+                    if (questionNum != 50)
+                    {
+                        questionNum++;
+                        timeLeft += 5;
+                        timeLabel.Text = $"{Math.Floor((double)timeLeft / 60)}:{((timeLeft % 60 > 9) ? Math.Round((double)timeLeft % 60) : "0" + Math.Round((double)timeLeft % 10))}";
+                        questionLabel.Text = "Q " + questionNum;
+                        questionAnswer = Generate_Question();
+                    }
+                    else
+                    {
+                        equationLabel.Text = "You Win!";
+                        buttonCont.Enabled = false;
+                        numericAnswer.Enabled = false;
+                        timer.Stop();
+                    }
+                }
+            }
+        }
+
+        private void timer_Tick(object sender, EventArgs e)
+        {
+            if (timeLeft > 0)
+            {
+                timeLeft--;
+                timeLabel.Text = $"{Math.Floor((decimal)timeLeft/60)}:{Math.Round((decimal)timeLeft%60)}";
+            }
+            else
+            {
+                timer.Stop();
+                timeLabel.Text = "Time's up!";
+                equationLabel.Text = "You Lose, Time's Up!";
+                buttonCont.Enabled = false;
+                numericAnswer.Enabled = false;
+            }
+        }
+    }
+}
